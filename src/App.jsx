cf16823a3554efa2278b87f2bece7e2b71ff4a49
KODAVATISANJAY <168344766+KODAVATISANJAY@users.noreply.github.com>
2025-12-12 21:54:05 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import VendorShowcase from './components/VendorShowcase'
+import ServicePackages from './components/ServicePackages'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-rose-50">
+      <Navbar />
+      <Hero />
+      <VendorShowcase />
+      <ServicePackages />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
