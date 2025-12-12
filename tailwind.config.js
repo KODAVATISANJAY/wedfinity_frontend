b081391f9018a5ef05b28b104744c66ba1020f33
KODAVATISANJAY <168344766+KODAVATISANJAY@users.noreply.github.com>
2025-12-12 21:52:44 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E91E63',
+        secondary: '#FFB6C1',
+        gold: '#FFD700',
+        rose: '#F3E5F5',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
